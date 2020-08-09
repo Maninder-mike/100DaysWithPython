30bebac974f17494b2385289f42493c9b1d8f268
Maninder.Mike <maninder.singh@outlook.in>
2020-08-09 19:45:27 +0300

    add second day commit

---
+++ 1 Day/dictionary.py
@@ -0,0 +1,35 @@
+x = {"key1": "value1", "key2": "value2"}
+z = dict(one=1, two=2, three=3)
+
+nested = {'data': {'name':"maninder",'age':25,'hobbies':['coding','writting','learning','bookworm']}}
+
+# print(z.items())
+# print(z.keys())
+# print(z.values())
+
+# print(z['one'])
+# print(nested['data']['name'])
+# print(nested['data']['hobbies'][0])
+
+
+# print("pop item: ", z.popitem())
+# print("pop", z.pop('two'))
+
+# y = x.copy()
+# print(y)
+
+# i = 0
+# g = ('one','two','three')
+# print(dict.fromkeys(g, i))
+
+
+# setdefuault = (x.setdefault('key3','value3'))
+# print(setdefuault)
+
+# update = x.update({'key2':'updated value'})
+# print(update)
+
+
+# print("clear: ", x.clear())
+
+
---
+++ 1 Day/tuple.py
@@ -0,0 +1,10 @@
+m = ('mike', 'tom', 'jerry')
+
+
+# how many times value appears in the tuple
+# print(m.count('tom'))
+
+
+# index of value
+# print(m.index('jerry'))
+
