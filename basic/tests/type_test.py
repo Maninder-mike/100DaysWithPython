d55c0c2bf83feff9132dfa0ab580caf484c9e417
Maninder.Mike <maninder.singh@outlook.in>
2020-08-16 21:36:59 +0300

    formating

--- basic/tests/type_test.py
+++ basic/tests/type_test.py
@@ -1,10 +1,7 @@
 m = "mike"
 
-from ..datatypee import int_value, string_value
 
 def test_types():
-    assert type(int_value) is int
-    assert type(string_value) is not int
 
-
-
+    assert type(m) is str
+    assert type(m) is not int
